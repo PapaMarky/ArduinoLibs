6413458c45f298b5f9fef9bd7b95b63a11105a63
PapaMarky <papamarky@markyshouse.com>
2013-04-19 21:30:06 -0700

    Move BoozeSensor to its own file

--- Booze_O_Meter/Booze_O_Meter.h
+++ Booze_O_Meter/Booze_O_Meter.h
@@ -6,44 +6,11 @@
 #include "MultiColorLED.h"
 #include "Button.h"
 #include "StateMachine.h"
+#include "BoozeSensor.h"
 
 #include <SoftwareSerial.h>
 
 namespace BOM {
-
-class BoozeSensor {
- public:
-  BoozeSensor();
-
-  void set_pins(int control, int data, int temperature);
-  
-  void turnOn() { control_.turnOn(); }
-  void turnOff() { control_.turnOff(); }
-
-  bool isOn() { return control_.getState(); }
-  
-  void set_state(bool b) { control_.set_state(b); }
-
-  int getTemperature();
-  
-  int read() {
-    return data_.read();
-  }
-
-  void setup() {
-    control_.setup();
-    data_.setup();
-    thermistor_.setup();
-  }
-
-  float CalculateAlcoholPercent();
-  int RawAlcoholValue() { return data_.read(); }
-  
- private:
-  mdlib::DigitalOutput control_;
-  mdlib::AnalogInput data_;
-  mdlib::AnalogInput thermistor_;  
-};
 
 class StateContext {
  public:
